62efdfc58d735a4f1ae3390c8d7f65b4b0f8223b
Rayees Riyaz <rayeesriyaz343@gmail.com>
2025-09-29 08:48:09 +0530

    Add Login errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,31 +2,30 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
-    user = User.find_by(name: params[:name])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to applications_path, notice: "Logged in successfully!"
-    else
-      flash.now[:alert] = "Invalid email or password"
-      render :new
-    end
+def create
+  user = User.find_by(name: params[:name])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect_to applications_path, notice: "Logged in successfully!"
+  else
+    flash.now[:alert] = "Invalid email or password"
+    render :new, status: :unprocessable_entity
   end
+end
 
-  def student_login
+def student_login
+end
+
+def student_create
+  user = User.find_by(registration_number: params[:registration_number])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect_to index_path, notice: "Logged in successfully!"
+  else
+    flash.now[:alert] = "Invalid Registration Number or Password"
+    render :student_login, status: :unprocessable_entity
   end
-
-  def student_create
-    user = User.find_by(registration_number: params[:registration_number])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to index_path, notice: "Logged in successfully!"
-    else
-      flash.now[:alert] = "Invalid Registration Number or Password"
-      render :student_login
-    end
-  end
-
+end
   def destroy
     session[:user_id] = nil
     redirect_to root_path, notice: "Logged out successfully!"
